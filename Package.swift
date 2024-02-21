0d436bf35e27d512c39164b21ddca15ceeaa4599
OhKanghoon <ggaa96@naver.com>
2024-02-21 23:45:16 +0900

    update package

--- Package.swift
+++ Package.swift
@@ -1,39 +1,45 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 import PackageDescription
 
 let package = Package(
-    name: "IGListKit",
-    platforms: [ .iOS(.v11),
-                 .tvOS(.v11),
-                 .macOS(.v10_13),
-    ],
-    products: [
-        .library(name: "IGListDiffKit",
-                 type: .static,
-                 targets: ["IGListDiffKit"]),
-        .library(name: "IGListKit",
-                 type: .static,
-                 targets: ["IGListKit"]),
-        .library(name: "IGListSwiftKit",
-                 type: .static,
-                 targets: ["IGListSwiftKit"]),
-    ],
-    targets: [
-        .target(
-            name: "IGListDiffKit",
-            path: "spm/Sources/IGListDiffKit"
-        ),
-        .target(
-            name: "IGListKit",
-            dependencies: ["IGListDiffKit"],
-            path: "spm/Sources/IGListKit"
-        ),
-        .target(
-            name: "IGListSwiftKit",
-            dependencies: ["IGListKit"],
-            path: "Source/IGListSwiftKit"
-        ),
-    ],
-    cLanguageStandard: .c11,
-    cxxLanguageStandard: .cxx11
+  name: "IGListKit",
+  platforms: [
+    .iOS(.v11),
+    .tvOS(.v11),
+    .macOS(.v10_13),
+  ],
+  products: [
+    .library(
+      name: "IGListDiffKit",
+      targets: ["IGListDiffKit"]
+    ),
+    .library(
+      name: "IGListKit",
+      targets: ["IGListKit"]
+    ),
+    .library(
+      name: "IGListSwiftKit",
+      targets: ["IGListSwiftKit"]
+    ),
+  ],
+  targets: [
+    .target(
+      name: "IGListDiffKit",
+      path: "spm/Sources/IGListDiffKit",
+      publicHeadersPath: "include"
+    ),
+    .target(
+      name: "IGListKit",
+      dependencies: ["IGListDiffKit"],
+      path: "spm/Sources/IGListKit",
+      publicHeadersPath: "include"
+    ),
+    .target(
+      name: "IGListSwiftKit",
+      dependencies: ["IGListKit"],
+      path: "Source/IGListSwiftKit"
+    ),
+  ],
+  cLanguageStandard: .c11,
+  cxxLanguageStandard: .cxx11
 )
